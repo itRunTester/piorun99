5e957c3f04ab9d804e3755a269d371f2ed7faec0
Runner <itrun.tester@gmail.com>
2023-02-14 20:31:58 +0100

    WWW-Authenticate3

--- app.py
+++ app.py
@@ -12,20 +12,14 @@ def check_auth(username, password):
     return username == USERNAME and password == PASSWORD
 
 
-# Endpoint wyświetlający formularz logowania
-@app.route('/', methods=['GET'])
-def login():
-    return Response('Nieprawidłowe dane logowania', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-
 # Funkcja chroniona uwierzytelnianiem
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def index():
     auth = request.authorization
     if not auth or not check_auth(auth.username, auth.password):
         return Response('Nieprawidłowe dane logowania', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
-    # return '<h1>Zalogowano pomyślnie!</h1>'
-    return render_template('index.html')
+    else:
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
